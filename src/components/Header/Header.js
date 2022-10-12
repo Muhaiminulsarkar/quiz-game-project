1bd6b5a388ef0d3667aa5b43c453c902b0d1b1ed
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2022-10-12 22:01:13 +0600

    blog part added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,15 +8,15 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <div>
-      <Navbar bg="light" expand="lg">
+      <Navbar bg="white" expand="lg">
         <Container>
           <Navbar.Brand className='fw-bold fs-3'>QuizGame</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mx-auto">
-              <Nav.Link className='fw-bold' as={Link} to={"/"}>Topics</Nav.Link>
-              <Nav.Link className='fw-bold' as={Link} to={"/statistics"}>Statistics</Nav.Link>
-              <Nav.Link className='fw-bold' as={Link} to={"/blog"}>Blog</Nav.Link>
+              <Nav.Link className='fw-bold fs-5' as={Link} to={"/"}>Topics</Nav.Link>
+              <Nav.Link className='fw-bold fs-5' as={Link} to={"/statistics"}>Statistics</Nav.Link>
+              <Nav.Link className='fw-bold fs-5' as={Link} to={"/blog"}>Blog</Nav.Link>
 
             </Nav>
           </Navbar.Collapse>
